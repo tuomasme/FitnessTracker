90aec824fe906fd94ae4a43e4482fb8c796db8e4
tuomasme <mertanentuomas@gmail.com>
2023-01-19 18:33:52 +0200

    Database connection

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from "express"
+import bodyParser from "body-parser"
+import mongoose from "mongoose"
+import cors from "cors"
+import dotenv from "dotenv"
+import path from "path"
+
+const app = express()
+dotenv.config()
+app.use(express.json())
+app.use(bodyParser.json({limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+app.use(cors())
+
+const PORT = process.env.PORT || 5500
+mongoose.connect(process.env.URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then (() => {
+    app.listen(PORT, () => console.log("Server connected"))
+}).catch((error) => console.log`${error} NO connection`)
